e51c278e642181109cbf390d140dce1607ec60ae
Alexander <dejifagbohun@live.co.uk>
2023-09-14 22:48:17 +0200

    Initial commit

---
+++ Login system/src/IDandPassword.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class IDandPassword {
+
+    HashMap<String,String> logininfo = new HashMap<String,String>();
+
+    IDandPassword(){
+
+        logininfo.put("Bro", "pizza");
+        logininfo.put("Brometheus", "PASSWORD");
+        logininfo.put("BroCode", "abc123");
+    }
+
+    protected HashMap getLoginInfo(){
+        return logininfo;
+    }
+}
